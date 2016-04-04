d85a6df96d73c904c22a8394195cb4100a9d2ddf
Matej Mojzes <matejmojzes@gmail.com>
2016-04-04 23:42:16 +0200

    Materials for 4/5/2016

---
+++ notebooks/20160405_Own_objective_function.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# # Implement your own objective function to test and tune heuristics on
+# 
+# * [Change-Making Problem](http://en.wikipedia.org/wiki/Change-making_problem)
+# * [Clustering](http://en.wikipedia.org/wiki/Cluster_analysis), [Sociogram](http://en.wikipedia.org/wiki/Sociogram) as an example
+# * [Knapsack Problem](http://en.wikipedia.org/wiki/Knapsack_problem) - either as 0/1, or $\mathbb{Z}^n$ one
+# * [Maximum Clique Problem](http://en.wikipedia.org/wiki/Clique_problem)
+# * [Set Covering Problem](http://en.wikipedia.org/wiki/Set_cover_problem)
+# * [Travelling Salesman Problem](http://en.wikipedia.org/wiki/Travelling_salesman_problem) 
+#   * with other geometry than currently implemented grid, or
+#   * let's try reasonably large instances from [TSPLIB](http://elib.zib.de/pub/mp-testdata/tsp/tsplib/tsplib.html)
