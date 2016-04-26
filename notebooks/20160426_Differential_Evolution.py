3c8bd980dab3ed36e24c7a4d86e0e8a43bbce57b
Matej Mojzes <matejmojzes@gmail.com>
2016-04-26 09:00:32 +0200

    Materials for 4/26/2016

---
+++ notebooks/20160426_Differential_Evolution.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# # Differential Evolution
+# 
+# Reccommended excercises:
+# 
+# 1. **Implement basic (continuous) DE**, which is well defined on [Wikipedia](https://en.wikipedia.org/wiki/Differential_evolution)
+# 2. **Implement contiuous version of Random Shooting**
+# 3. **Compare on benchmark objective functions** defined on http://www.geatbx.com/docu/fcnindex-01.html, e.g.:
+#    1. De Jong's function 1
+#    2. Rastrigin's function 6
